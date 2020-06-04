4736de2809b3a91551e8283e92ffc9cc997e6698
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-03 19:32:57 -0600

    Add pseudocode for word map challenge

--- wordmap.js
+++ wordmap.js
@@ -1,6 +1,13 @@
 //You want to build a word cloud, an infographic where the size of a word corresponds to how often it appears in the body of text.
 //Utilize a map
 
+//<--Pseudocode--> 
+//input, string of words
+//output, wordsToCounts property should contain a map with words as keys, and number of occurences as their values
+
+//want to split on spaces, iterate over words. Lowercase those words and then add them to the map. 
+//if present set value = value + 1
+
 //Given structure
 class WordCloudData {
     constructor(inputString) {
@@ -9,7 +16,6 @@ class WordCloudData {
     }
 
     populateWordsToCounts(inputString) {
-
         // Count the frequency of each word
 
 
