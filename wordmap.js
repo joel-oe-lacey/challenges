2702ed3b6bb5c5f5c5ddd5a6dc9f64b4c8906e12
Joel Lacey <joellaceybusiness@gmail.com>
2020-06-03 19:27:37 -0600

    Add problem foundation

---
+++ wordmap.js
@@ -0,0 +1,18 @@
+//You want to build a word cloud, an infographic where the size of a word corresponds to how often it appears in the body of text.
+//Utilize a map
+
+//Given structure
+class WordCloudData {
+    constructor(inputString) {
+        this.wordsToCounts = new Map();
+        this.populateWordsToCounts(inputString);
+    }
+
+    populateWordsToCounts(inputString) {
+
+        // Count the frequency of each word
+
+
+    }
+
+}
