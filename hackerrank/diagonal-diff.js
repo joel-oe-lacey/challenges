c1c247ce25a68c67161d4efd1bf5703c0c72de05
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-28 18:06:26 -0500

    Add diagonal diff solution

--- hackerrank/diagonal-diff.js
+++ hackerrank/diagonal-diff.js
@@ -9,3 +9,17 @@
 // take first and last element, add respectively to left & right sum
 // on next i + 1, len - 1
 // take difference of sums and return 
+
+function diagonalDifference(arr) {
+    let primarySum = 0;
+    let secondarySum = 0;
+    let n = arr[0].length - 1;
+
+    for (let i = 0; i < arr.length; i++) {
+        primarySum += arr[i][i]
+        secondarySum += arr[i][n]
+        n--
+    }
+
+    return Math.abs(primarySum - secondarySum)
+}
