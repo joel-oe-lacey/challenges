b8f9897dcb46aba7b213d97e9937e266b49bd21e
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-28 18:26:10 -0500

    Add str reduce problem and pseudocode

---
+++ hackerrank/reduced-str.js
@@ -0,0 +1,8 @@
+// Problem: https://www.hackerrank.com/challenges/reduced-string/problem
+
+// Pseudocode
+// could perform same task repeatedly, checking for duplicate char and removing
+// repeat until fail, good case for recursion
+// alternatively with a single pass you could map all characters
+// if even remove
+// if odd, leave 1
