3c06d65d0bf0d96b84caae1c3cf8df58e6957346
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-28 17:36:42 -0500

    Add diagonal diff problem and pseudocode

---
+++ hackerrank/diagonal-diff.js
@@ -0,0 +1,11 @@
+// Problem outlined: https://www.hackerrank.com/challenges/diagonal-difference/problem?h_r=next-challenge&h_v=zen
+
+
+// Pseudocode
+// Input a 2D int array, first element dictactes row and column count
+// subsequent elements are arrays representing rows
+// we then want to fetch diagonals
+// nested for loops, start i = 1
+// take first and last element, add respectively to left & right sum
+// on next i + 1, len - 1
+// take difference of sums and return 
