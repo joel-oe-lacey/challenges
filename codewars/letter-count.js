f6cdfaa463285d2ffdcc59a1d0422dc242e16689
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-29 16:43:19 -0500

    Add letter count problem and pseudocode

---
+++ codewars/letter-count.js
@@ -0,0 +1,9 @@
+// Problem: https://www.codewars.com/kata/5b358a1e228d316283001892
+
+// Pseudocode
+// create a string tracker object
+// split string, lowercase letter, place it into tracker
+// at the end recompose tracker into string, reduce
+// repeat to compose stars 
+// need to preserve some sort of order however
+// use a Map 
