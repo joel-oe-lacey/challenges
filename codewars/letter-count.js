b5d1ea5e186a9b0634f5778a93f6230f26fde3d7
Joel Lacey <joellaceybusiness@gmail.com>
2020-12-29 17:30:52 -0500

    Submit letter-count solution

--- codewars/letter-count.js
+++ codewars/letter-count.js
@@ -7,3 +7,25 @@
 // repeat to compose stars 
 // need to preserve some sort of order however
 // use a Map 
+
+function getStrings(city){
+    const charMap = new Map();
+    const re = /[A-Za-z+]/g;
+    
+    let result = '';
+
+    for (let i = 0; i < city.length; i++) {
+        if (charMap.has(city[i])) {
+            const charCountIterated = charMap.get(city[i])++;
+            charMap.set(city[i], charCountIterated)
+        } else {
+            charMap.set(city[i], 1)
+        }
+    }
+
+    charMap.forEach((val, key) => {
+        result += `${key}:${'*'.repeat(val)},`
+    })
+
+    return result.substring(0, result.length - 1);
+}
