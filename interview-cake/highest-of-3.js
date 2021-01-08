991f590b53f013d7402399f26e891b21c4d36c56
Joel Lacey <joellaceybusiness@gmail.com>
2021-01-08 10:36:55 -0500

    Add negative trackign solution highest-3

--- interview-cake/highest-of-3.js
+++ interview-cake/highest-of-3.js
@@ -15,28 +15,68 @@
 // if not, check if bigger than last, if so replace last 
 // splicing into middle of arr bad for space complexity 
 
+// this doesn't properly account for negatives
+// to change this we'll also need to account for negatives 
+
 // highest product 966
 const exampleInput = [5, 14, 8, 22, 31, 4, 913]
+// highest product 300
+const secondaryInput = [-10, -10, 4, 3]
+
+// const findHighestProduct = numArr => {
+//     const maxValues = [Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
+
+//     for (i = 0; i < numArr.length; i++) {
+//         if (numArr[i] > maxValues[0]) {
+//             maxValues.unshift(numArr[i]);
+//             maxValues.pop();
+//         } else if (numArr[i] > maxValues[1]) {
+//             maxValues.splice(1, 0, numArr[i])
+//             maxValues.pop();
+//         } else if (numArr[i] > maxValues[2]) {
+//             maxValues[2] = numArr[i]
+//         }
+
+//         console.log(maxValues)
+//     }
+
+//     return maxValues[0] * maxValues[1] * maxValues[2];
+// }
 
 const findHighestProduct = numArr => {
-    const maxValues = [Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER, Number.MIN_SAFE_INTEGER];
+  let highest = Math.max(numArr[0], numArr[1]);
+  let lowest  = Math.min(numArr[0], numArr[1]);
 
-    //operate in reverse 
-    for (i = 0; i < numArr.length; i++) {
-        if (numArr[i] > maxValues[0]) {
-            maxValues.unshift(numArr[i]);
-            maxValues.pop();
-        } else if (numArr[i] > maxValues[1]) {
-            maxValues.splice(1, 0, numArr[i])
-            maxValues.pop();
-        } else if (numArr[i] > maxValues[2]) {
-            maxValues[2] = numArr[i]
-        }
+  let highestProductOf2 = numArr[0] * numArr[1];
+  let lowestProductOf2  = numArr[0] * numArr[1];
 
-        console.log(maxValues)
-    }
+  let highestProductOf3 = numArr[0] * numArr[1] * numArr[2];
 
-    return maxValues[0] + maxValues[1] + maxValues[2];
+  for (let i = 2; i < numArr.length; i++) {
+    highestProductOf3 = Math.max(
+      highestProductOf3,
+      numArr[i] * highestProductOf2,
+      numArr[i] * lowestProductOf2
+    );
+
+    highestProductOf2 = Math.max(
+      highestProductOf2,
+      numArr[i] * highest,
+      numArr[i] * lowest
+    );
+
+    lowestProductOf2 = Math.min(
+      lowestProductOf2,
+      numArr[i] * highest,
+      numArr[i] * lowest
+    );
+
+    highest = Math.max(highest, numArr[i]);
+
+    lowest = Math.min(lowest, numArr[i]);
+  }
+
+  return highestProductOf3;
 }
 
 console.log(findHighestProduct(exampleInput))
