391c55d38c4b2964d3170d027d272f7b9b9e44ed
Joel Lacey <joellaceybusiness@gmail.com>
2021-01-07 22:31:26 -0500

    Add highest of 3 product problem and pseudocode

---
+++ interview-cake/highest-of-3.js
@@ -0,0 +1,11 @@
+// Given an array of integers, find the highest product you can get from three of the integers.
+// The input arrayOfInts will always have at least three integers.
+
+// Pseudocode
+// We have an array of 3+ ints
+// want to find highest product 
+// iterate over starting at three, i === 2
+// track first three numbers
+// as we do we want to check if the next number is bigger than any of our current numbers
+// if so replace lowest with new number
+// repeat until end
